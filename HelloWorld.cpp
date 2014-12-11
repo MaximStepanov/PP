57b4212ed4cd6077d66fd29d4d3a753cf5e25c7c
Alone4ik <kiper94@mail.ru>
2014-12-11 15:06:47 +0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	if (rank == 0)
 	{
 		cout<<"Hello from process_"<<rank<<endl;
-		for (int i = 0; i < size; i++)
+		for (int i = 1; i < size; i++)
 		{
 			MPI_Recv(&RecvRank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			cout<<"Hello from process "<<RecvRank<<endl;
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
 	} else
 	{
 		MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		MPI_Finalize();
 	}
+	MPI_Finalize();
 	return 0;
 }
