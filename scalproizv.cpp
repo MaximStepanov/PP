97cf05648b3512be05a875c2d103b1d80765d3c4
Alone4ik <kiper94@mail.ru>
2014-12-15 03:05:20 +0400

    Update scalproizv.cpp

--- scalproizv.cpp
+++ scalproizv.cpp
@@ -14,16 +14,15 @@ int main (int argc, char * argv[])
 	int rank, size, total, n, dest;
 	int *a, *b, sum, result;
 	MPI_Status status;
-	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
 	if (rank == 0) 
 	{
-		cout << "Введите длину векторов" << endl;
-		cin >> total;
-		n = total / size + 1;
+ 	cout << "Введите длину векторов" << endl;
+	cin >> total;
+	n = total / size + 1;
 	}
 	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	a = (int*) malloc(n * sizeof(int));
